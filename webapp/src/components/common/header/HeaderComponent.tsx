92740765cc378fa828c3a7309ebfa54e2f350edb
Julio Perez Viloria <protobot.dev@gmail.com>
2022-05-08 10:58:43 -0300

    fix to build the final header

--- webapp/src/components/common/header/HeaderComponent.tsx
+++ webapp/src/components/common/header/HeaderComponent.tsx
@@ -1,50 +1,31 @@
-import {useEffect} from "react";
-import {useMediaQuery} from "react-responsive";
 import "./HeaderComponent.css";
 
-//import homeIcon from "../../../../public/home.png";
-
-const MobileHeader = ({children}) =>{
-    const isMobile = useMediaQuery({maxWidth: 767})
-    return isMobile ? children : null;
-}
-
-const DesktopHeader = ({children}) =>{
-    const isMobile = useMediaQuery({minWidth: 768})
-    return isMobile ? children : null;
-}
-
 export const HeaderComponent = () => {
-    useEffect(() =>{},[MobileHeader, DesktopHeader])
     return(
     <div>
-        <MobileHeader>
-            <div className="mobileHeader">
-                <img 
-                src="/image/home.png"
-                className="headerIcon"/>
-                <img 
-                src="/image/dollar.png"
-                className="headerIcon"/>
-                <img 
-                src="/image/heart.png"
-                className="headerIcon"/>
-                <img 
-                src="/image/address.png"
-                className="headerIcon"/>
+        <div className="mobileHeader">
+            <img 
+            src="/image/home.png"
+            className="headerIcon"/>
+            <img 
+            src="/image/dollar.png"
+            className="headerIcon"/>
+            <img 
+            src="/image/heart.png"
+            className="headerIcon"/>
+            <img 
+            src="/image/address.png"
+            className="headerIcon"/>
+        </div>
+        <div className="desktopHeader">
+            <h1>juliopredictor</h1>
+            <div>
+                <p>Home</p>
+                <p>Cryptos</p>
+                <p>Favorites</p>
+                <p>Perfil</p>
             </div>
-        </MobileHeader>
-        <DesktopHeader>
-            <div className="desktopHeader">
-                <h1>juliopredictor</h1>
-                <div>
-                    <p>Home</p>
-                    <p>Cryptos</p>
-                    <p>Favorites</p>
-                    <p>Perfil</p>
-                </div>
-            </div>
-        </DesktopHeader>
+        </div>
     </div>
     )
 }

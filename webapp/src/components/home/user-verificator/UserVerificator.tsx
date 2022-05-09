e32175f2ff64617961da334f9868f3f65e1eccc9
Julio Perez Viloria <protobot.dev@gmail.com>
2022-05-09 00:04:32 -0300

    implement authenticator component with mobile first

---
+++ webapp/src/components/home/user-verificator/UserVerificator.tsx
@@ -0,0 +1,63 @@
+import "./UserVerificator.css"
+
+export const UserVerificator = ({authenticationStatus}) =>{
+
+    const stringCreator = (value:string, quantity:number):string =>{
+        return value.length > quantity ? value.slice(0, quantity).concat("..."): value;
+    }
+
+    return(
+        <div 
+        className={"user-verificator-base user-verificator-".concat(authenticationStatus.toString())}>
+            {(() => {
+                switch(authenticationStatus){
+                case 0:
+                    return <>
+                    <div className="">
+                        <img 
+                        src="image/add.png" 
+                        className="" />
+                        <p>Agregar email</p>
+                    </div>
+                    <div>
+                        <img 
+                        src="image/cross.png" 
+                        alt="" />
+                        <p>Sin verificacion</p>
+                    </div>
+                    </>    
+
+                case 1:
+                    return <>
+                    <div>
+                        <img 
+                        src="image/user3.png" 
+                        className="" />
+                        <p>{stringCreator("perezjulioernesto@gmail.comfsdfsdfsdfsdfsfs", 30)}</p>
+                    </div>
+                    <div>
+                        <img 
+                        src="image/clock5.png" 
+                        alt="" />
+                        <p>A espera de validacion</p> 
+                    </div>
+                    </>
+                case 2:
+                    return <>
+                    <div>
+                        <img 
+                        src="image/user2.png" 
+                        className="" />
+                        <p>perezjulioernesto@gmail.com</p>
+                    </div>
+                    <div>
+                        <img 
+                        src="image/check.png" 
+                        alt="" />
+                        <p>Verificado</p> 
+                    </div>
+                    </>
+                }})()}
+        </div>
+    )
+}
--- webapp/src/pages/publics/home/HomePage.tsx
+++ webapp/src/pages/publics/home/HomePage.tsx
@@ -1,11 +1,24 @@
+import {UserVerificator} from "../../../components/home/user-verificator/UserVerificator"
+
+import "./HomePage.css"
 
 export const HomePage = () =>{
+    
+    const UNAUTHENTICATED : number = 0;
+    const AUTHENTICATION_IN_PROCESS : number = 1;
+    const AUTHENTICATED : number = 2;
+
+    const authenticationStatus : number = 1;
     return(
         <>
-        <p>userComponent</p>
+        <div className="home-page-base">
+            <UserVerificator 
+            authenticationStatus ={authenticationStatus}/>
+        </div>
+        {/* <p>userComponent</p>
         <p>favoriteList</p>
         <p>top5Currencies</p>
-        <p>socialMedia</p>
+        <p>socialMedia</p> */}
         </>
     )
 }
